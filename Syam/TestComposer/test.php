063f63dbb1c1c178d0f6f49c62828138ef11ef04
syam.sg <syam.sg@pitsolutions.com>
2024-10-22 17:13:48 +0530

    initial commit

---
+++ Syam/TestComposer/test.php
@@ -0,0 +1,5 @@
+<tr class="totals shipping excl">
+        <th class="mark" scope="row"> 
+            <span class="value" data-bind="text: getShippingMethodTitle()"></span>
+        </th>
+    </tr>
